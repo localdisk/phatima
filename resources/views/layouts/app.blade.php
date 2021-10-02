8160e001385fbea9a092002261f674e7bb5736e6
localdisk <smoochynet@gmail.com>
2021-10-03 00:28:38 +0900

    login form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,18 +5,19 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>@yield('title', config('app.name'))</title>
-  <livewire:styles />
+  @livewireStyles
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   @stack('styles')
 </head>
 
-<body class="h-full w-full">
+<body>
 
   @yield('content')
 
-  <livewire:scripts />
+  @livewireScripts
   <script src="{{ mix('js/app.js') }}"></script>
   @stack('scripts')
+
 </body>
 
 </html>
---
+++ resources/views/livewire/login-form.blade.php
@@ -0,0 +1,16 @@
+<div class="flex items-center justify-center h-screen w-screen">
+  <div class="w-1/5">
+    <h1 class="text-2xl">ログイン</h1>
+    <label class="block my-1 text-lg">
+      ユーザー名
+      <input type="text" class="w-full">
+    </label>
+    <label class="block my-1 text-lg">
+      パスワード
+      <input type="password" class="w-full">
+    </label>
+    <p class="my-3">
+      <button class="p-3 w-full shadow-lg bg-blue-600 text-white">ログイン</button>
+    </p>
+  </div>
+</div>
