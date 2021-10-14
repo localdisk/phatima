fe8380bc44f70b12e63e2d2577c78176bc7f1bfe
localdisk <smoochynet@gmail.com>
2021-10-14 22:55:38 +0900

    validation

--- app/Http/Livewire/RegisterForm.php
+++ app/Http/Livewire/RegisterForm.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App\Http\Livewire;
 
@@ -6,6 +6,24 @@ use Livewire\Component;
 
 class RegisterForm extends Component
 {
+    public string $name = '';
+    public string $email = '';
+    public string $password = '';
+    public string $password_confirmation = '';
+
+    protected $rules = [
+        'name' => ['required', 'string'],
+        'email' => ['required', 'string', 'email'],
+        'password' => ['required', 'string', 'confirmed', 'min:8', 'same:password_confirmation'],
+        'password_confirmation' => ['required', 'string']
+    ];
+
+    public function register()
+    {
+        $this->validate();
+
+        $this->reset();
+    }
     public function render()
     {
         return view('livewire.register-form');
