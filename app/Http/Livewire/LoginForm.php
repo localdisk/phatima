f4849f26b9a800f5ed2b99db486af3882a7dd29b
localdisk <smoochynet@gmail.com>
2021-10-17 15:37:11 +0900

    login

--- app/Http/Livewire/LoginForm.php
+++ app/Http/Livewire/LoginForm.php
@@ -2,17 +2,37 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class LoginForm extends Component
 {
-    public string $user = '';
+    public string $email = '';
 
     public string $password = '';
 
-    public function login(): void
-    {
+    protected $rules = [
+        'email' => ['required', 'string', 'email'],
+        'password' => ['required', 'string', 'min:8'],
+    ];
 
+    public function updated(string $property): void
+    {
+        $this->validateOnly($property);
+    }
+
+    public function login()
+    {
+        $this->validate();
+
+        if (Auth::attempt([
+            'email' => $this->email,
+            'password' => $this->password,
+        ])) {
+            return redirect('dashboard');
+        }
+
+        $this->addError('login', 'ログインエラー');
     }
 
     public function render()
--- resources/views/livewire/login-form.blade.php
+++ resources/views/livewire/login-form.blade.php
@@ -1,12 +1,15 @@
 <div class="flex items-center justify-center h-screen w-screen">
   <div class="md:w-1/5 w-full p-2">
+    <p>@error('login') <p class="text-red-500">{{ $message }}</p> @enderror</p>
     <label class="block my-1 text-lg">
-      ユーザー名
-      <input type="text" wire:model="user" class="w-full">
+      メールアドレス
+      <input type="text" wire:model.lazy="email" class="w-full">
+      @error('email') <p class="text-red-500">{{ $message }}</p> @enderror
     </label>
     <label class="block my-1 text-lg">
       パスワード
-      <input type="password" wire:model="password" class="w-full">
+      <input type="password" wire:model.lazy="password" class="w-full">
+      @error('password') <p class="text-red-500">{{ $message }}</p> @enderror
     </label>
     <p class="my-3">
       <button wire:click="login" class="p-3 w-full shadow-lg bg-blue-600 text-white">ログイン</button>
