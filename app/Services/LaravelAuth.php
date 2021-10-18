091c02b088d2c844dbd02efff240751ff9bf2e2a
localdisk <smoochynet@gmail.com>
2021-10-18 23:35:58 +0900

    create LaravelAuth

--- app/Services/LaravelAuth.php
+++ app/Services/LaravelAuth.php
@@ -9,32 +9,35 @@ use Illuminate\Support\Str;
 
 class LaravelAuth implements AuthInterface
 {
-    const PER_MINUTS = 5;
+    const MAX_ATTEMPTS = 5;
 
     public function login(string $email, string $password): bool {
-        if (! Auth::attempt([
-            'email' => $this->email,
-            'password' => $this->password,
-        ])) {
-            $this->addError('login', 'ログインエラー');
-            RateLimiter::hit($this->throttleKey());
-
-            throw ValidationException::withMessages([
-                'email' => __('auth.failed'),
-            ]);
+        if (Auth::attempt(['email' => $this->email, 'password' => $this->password])) {
+            $this->clearRateLimit();
+            return true;
         }
-        return true;
+
+        // ログイン失敗
+        $this->hitRateLimit($email);
+
+        return false;
      }
 
      private function hitRateLimit(string $email): void
      {
-         if (! RateLimiter::tooManyAttempts($this->throttleKey())) {
-             RateLimiter::hit($this->throttleKey());
-
-             return;
+        if (! RateLimiter::tooManyAttempts($this->throttleKey(), self::MAX_ATTEMPTS)) {
+            RateLimiter::hit($this->throttleKey());
+            return;
          }
 
          event(new Lockout(request()->merge(['email' => $this->email])));
+     }
+
+     private function clearRateLimit(): void
+     {
+         RateLimiter::clear($this->throttleKey());
+
+         return;
      }
 
      private function throttleKey()
