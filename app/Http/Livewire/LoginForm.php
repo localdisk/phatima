3f1bfbafcc8275b154a501def3979b956b3a92dc
localdisk <smoochynet@gmail.com>
2021-10-19 21:59:43 +0900

    format.

--- app/Http/Livewire/LoginForm.php
+++ app/Http/Livewire/LoginForm.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Http\Livewire;
 
 use Illuminate\Auth\Events\Lockout;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
