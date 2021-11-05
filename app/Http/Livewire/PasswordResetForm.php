492e91daf14de4d04d2a3a0a89e11b563f84c930
localdisk <smoochynet@gmail.com>
2021-11-06 03:02:36 +0900

    create password reset form

---
+++ app/Http/Livewire/PasswordResetForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class PasswordResetForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.password-reset-form');
+    }
+}
