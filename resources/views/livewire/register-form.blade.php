bb2560bb07691807f2602780d3654fe8526a3570
localdisk <smoochynet@gmail.com>
2021-11-11 20:27:47 +0900

    remove unused code.

--- resources/views/livewire/register-form.blade.php
+++ resources/views/livewire/register-form.blade.php
@@ -1,30 +1,27 @@
 <div class="flex items-center justify-center h-screen w-screen">
-    <div class="md:w-1/5 w-full p-2">
-      <label class="block my-1 text-lg">
-        {{ __('Username') }}
-        <input type="text" wire:model.defer="name" class="w-full">
-        @error('name') <p class="text-red-500">{{ $message }}</p> @enderror
-      </label>
-      <label class="block my-1 text-lg">
-        {{ __('Email') }}
-        <input type="text" wire:model.defer="email" class="w-full">
-        @error('email') <p class="text-red-500">{{ $message }}</p> @enderror
-      </label>
-      <label class="block my-1 text-lg">
-        {{ __('Password') }}
-        <input type="password" wire:model.defer="password" class="w-full">
-        @error('password') <p class="text-red-500">{{ $message }}</p> @enderror
-      </label>
-      <label class="block my-1 text-lg">
-        {{ __('Confirm Password') }}
-        <input type="password" wire:model.defer="password_confirmation" class="w-full">
-        @error('password_confirmation') <p class="text-red-500">{{ $message }}</p> @enderror
-      </label>
-      <p class="my-3">
-        <button wire:click="register" class="p-3 w-full shadow-lg bg-blue-600 text-white">登録</button>
-      </p>
-    </div>
+  <div class="md:w-1/5 w-full p-2">
+    <label class="block my-1 text-lg">
+      {{ __('Username') }}
+      <input type="text" wire:model.defer="name" class="w-full">
+      @error('name') <p class="text-red-500">{{ $message }}</p> @enderror
+    </label>
+    <label class="block my-1 text-lg">
+      {{ __('Email') }}
+      <input type="text" wire:model.defer="email" class="w-full">
+      @error('email') <p class="text-red-500">{{ $message }}</p> @enderror
+    </label>
+    <label class="block my-1 text-lg">
+      {{ __('Password') }}
+      <input type="password" wire:model.defer="password" class="w-full">
+      @error('password') <p class="text-red-500">{{ $message }}</p> @enderror
+    </label>
+    <label class="block my-1 text-lg">
+      {{ __('Confirm Password') }}
+      <input type="password" wire:model.defer="password_confirmation" class="w-full">
+      @error('password_confirmation') <p class="text-red-500">{{ $message }}</p> @enderror
+    </label>
+    <p class="my-3">
+      <button wire:click="register" class="p-3 w-full shadow-lg bg-blue-600 text-white">登録</button>
+    </p>
   </div>
-  @push('scripts')
-      aaa
-  @endpush
+</div>
