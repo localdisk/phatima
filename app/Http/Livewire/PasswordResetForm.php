9203f5bedba04eda441e5d73cedb6a8d30af65f3
localdisk <smoochynet@gmail.com>
2021-11-13 18:49:07 +0900

    add token and email property.

--- app/Http/Livewire/PasswordResetForm.php
+++ app/Http/Livewire/PasswordResetForm.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Http\Request;
 use Livewire\Component;
 
 class PasswordResetForm extends Component
 {
+    public string $token = '';
+
+    public string $email = '';
+
+    protected $queryString = ['token', 'email'];
+
     public function render()
     {
         return view('livewire.password-reset-form');
--- resources/views/livewire/password-reset-form.blade.php
+++ resources/views/livewire/password-reset-form.blade.php
@@ -1,3 +1,17 @@
-<div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+<div class="flex items-center justify-center h-screen w-screen">
+  <div class="md:w-1/5 w-full p-2">
+    <label class="block my-1 text-lg">
+      {{ __('Password') }}
+      <input type="password" wire:model.defer="password" class="w-full">
+      @error('password') <p class="text-red-500">{{ $message }}</p> @enderror
+    </label>
+    <label class="block my-1 text-lg">
+      {{ __('Confirm Password') }}
+      <input type="password" wire:model.defer="password_confirmation" class="w-full">
+      @error('password_confirmation') <p class="text-red-500">{{ $message }}</p> @enderror
+    </label>
+    <p class="my-3">
+      <button wire:click="register" class="p-3 w-full shadow-lg bg-blue-600 text-white">登録</button>
+    </p>
+  </div>
 </div>
