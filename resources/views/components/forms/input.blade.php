ca4e167d54798f77e0bd23f985acd39dd7352348
localdisk <smoochynet@gmail.com>
2023-08-04 01:10:08 +0900

    add inpurt component.

---
+++ resources/views/components/forms/input.blade.php
@@ -0,0 +1,2 @@
+<input type="text"
+    {{ $attributes->merge(['class' => 'rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 placeholder:text-gray-400 placeholder:italic']) }} />
