4f94edd69db36167312a1d5e64e21461cb44c74f
localdisk <smoochynet@gmail.com>
2023-08-04 01:10:30 +0900

    add admin layout.

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title', 'Admin')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="w-full h-full">
+    @yield('content')
+</body>
+
+</html>
