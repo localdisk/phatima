8e1156065a20c0581654312fdf5de125e0e71d0b
localdisk <smoochynet@gmail.com>
2023-08-24 11:28:32 +0900

    delete

--- resources/views/components/forms/input.blade.php
+++
@@ -1,2 +0,0 @@
-<input type="text"
-    {{ $attributes->merge(['class' => 'rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 placeholder:text-gray-400 placeholder:italic']) }} />
