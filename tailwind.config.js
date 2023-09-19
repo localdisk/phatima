88cec0ef50d2fcc4b3f1803066dc8d69e7858144
localdisk <smoochynet@gmail.com>
2023-09-19 21:04:08 +0900

    change theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,21 +14,7 @@ export default {
         extend: {},
     },
     daisyui: {
-        themes: [
-            {
-                mytheme: {
-                    "primary": "#E6E6E6",
-                    "secondary": "#F000B8",
-                    "accent": "#37CDBE",
-                    "neutral": "#3D4451",
-                    "base-100": "#FFFFFF",
-                    "info": "#3ABFF8",
-                    "success": "#36D399",
-                    "warning": "#FBBD23",
-                    "error": "#F87272",
-                },
-            },
-        ],
+        themes: ["cmyk", "dracura", "autumn"],
     },
     // Add daisyUI
     plugins: [
