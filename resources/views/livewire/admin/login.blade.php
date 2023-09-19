42761e161db299ed32efc86d90383c054673db35
localdisk <smoochynet@gmail.com>
2023-09-19 22:14:31 +0900

    fix

--- resources/views/livewire/admin/login.blade.php
+++ resources/views/livewire/admin/login.blade.php
@@ -1,9 +1,11 @@
 <div class="bg-gray-100">
+    @if (session('error'))
+        <div class="mb-6">
+            <x-alert title="ユーザー名かパスワードが間違っています" icon="o-exclamation-triangle" class="alert-error" />
+        </div>
+    @endif
     <form wire:submit="login" method="POST" class="w-screen h-screen flex justify-center items-center">
         <div class="bg-gray-100 w-1/5">
-            @error('email')
-                <x-button label="{{ __('auth.failed') }}" class="bg-red-400 w-full mb-6 text-white" />
-            @enderror
             <x-card title="Login" shadow separator class="w-full">
                 <div class="mb-6">
                     <x-input label="Email" wire:model="form.email" name="email" placeholder="Mail Address"
