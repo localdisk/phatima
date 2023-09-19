412ced4414f3c4edd8a696f8819300480aaa771d
localdisk <smoochynet@gmail.com>
2023-09-19 22:14:42 +0900

    fix

--- app/Livewire/Admin/Login.php
+++ app/Livewire/Admin/Login.php
@@ -4,32 +4,37 @@ declare(strict_types=1);
 
 namespace App\Livewire\Admin;
 
+use App\Livewire\Dashboard;
 use App\Livewire\Forms\Admin\LoginForm;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class Login extends Component
 {
     public LoginForm $form;
 
-    public function login(): Redirector|RedirectResponse
+    public function login()
     {
         $this->validate();
 
-        Auth::attempt([
+        if (Auth::attempt([
             'email' => $this->form->email,
             'password' => $this->form->password,
-        ], $this->form->remember);
+        ], $this->form->remember)) {
+            return $this->redirect(Dashboard::class);
+        }
 
-        return redirect(route('dashboard'));
+        session()->flash('error', 'ユーザー名かパスワードが間違っています');
+
+        return $this->redirect(Login::class);
     }
 
     #[Layout('layouts.guest')]
+    #[Title('Login')]
     public function render(): View|Factory
     {
         return view('livewire.admin.login');
