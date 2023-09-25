ddd80f5636e93fbc6d74009a79080e1f39236c5f
localdisk <smoochynet@gmail.com>
2023-09-25 19:35:10 +0900

    toast editor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,6 +72,19 @@
         </x-slot:content>
     </x-main>
     @stack('scripts')
+    {{-- <script src="https://uicdn.toast.com/editor/latest/toastui-editor-all.min.js"></script> --}}
+    {{-- <script>
+        var editor = new toastui.Editor({
+            el: document.querySelector('#editor')
+        });
+    </script> --}}
+
+    {{-- <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
+    <script>
+        var quill = new Quill('#editor', {
+            theme: 'snow'
+        });
+    </script> --}}
 </body>
 
 </html>
--- resources/views/livewire/admin/register-post.blade.php
+++ resources/views/livewire/admin/register-post.blade.php
@@ -1,17 +1,58 @@
 <div>
+    @push('css')
+        <link rel="stylesheet" href="https://uicdn.toast.com/editor/latest/toastui-editor.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/themes/prism.min.css" />
+        <link rel="stylesheet"
+            href="https://uicdn.toast.com/editor-plugin-code-syntax-highlight/latest/toastui-editor-plugin-code-syntax-highlight.min.css" />
+    @endpush
     <div>
         <form wire:submit="" method="POST">
             <x-card title="Register Post" shadow separator class="w-full shadow-md">
                 <div class="mb-6">
-                    <x-input wire:model="form.name" label="Title" placeholder="Title" type="text" class="no-border" />
+                    <x-input wire:model="form.name" label="Title" placeholder="Title" type="text"
+                        class="no-border" />
                 </div>
                 <div class="mb-6">
                     <x-input label="Email" wire:model="form.email" name="email" placeholder="Mail Address"
                         type="email" class="no-border" />
                 </div>
+                <div wire:ignore>
+                    <div id="editor"></div>
+                </div>
                 <x-button type="submit" label="Register" class="btn-primary w-full !text-white text-lg mt-6" />
             </x-card>
         </form>
-
     </div>
+    @push('scripts')
+        <script src="https://uicdn.toast.com/editor/latest/toastui-editor-all.min.js"></script>
+        <script
+            src="https://uicdn.toast.com/editor-plugin-code-syntax-highlight/latest/toastui-editor-plugin-code-syntax-highlight-all.min.js">
+        </script>
+        <script>
+            document.addEventListener('livewire:init', () => {
+                const {
+                    Editor
+                } = toastui;
+                const {
+                    codeSyntaxHighlight
+                } = Editor.plugin;
+                const editor = new Editor({
+                    el: document.querySelector('#editor'),
+                    initialEditType: 'markdown',
+                    toolbarItems: [],
+                });
+                // const {
+                //     Editor
+                // } = toastui;
+                // const {
+                //     codeSyntaxHighlight
+                // } = Editor.plugin;
+
+                // const instance = new Editor({
+                //     // ...
+                //     plugins: [codeSyntaxHighlight]
+                // });
+            })
+        </script>
+    @endpush
 </div>
