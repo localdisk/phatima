6a3e16b143161e45317dae0bf9cd570062b4e9c6
localdisk <smoochynet@gmail.com>
2023-09-27 01:53:54 +0900

    post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+
+    public function tags()
+    {
+        $this->belongsToMany(Tag::class);
+    }
 }
