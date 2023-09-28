94ed61b66e3136513f1f0a822e615bd74ecc075c
localdisk <smoochynet@gmail.com>
2023-09-28 22:41:51 +0900

    clean

--- resources/views/livewire/admin/register-post.blade.php
+++ resources/views/livewire/admin/register-post.blade.php
@@ -1,24 +1,12 @@
 <div>
     @push('css')
         <link rel="stylesheet" href="https://uicdn.toast.com/editor/latest/toastui-editor.min.css" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/themes/prism.min.css" />
-        <link rel="stylesheet"
-            href="https://uicdn.toast.com/editor-plugin-code-syntax-highlight/latest/toastui-editor-plugin-code-syntax-highlight.min.css" />
-        <script src="https://uicdn.toast.com/editor/latest/toastui-editor-all.min.js" data-navigate-track></script>
-        <script
-            src="https://uicdn.toast.com/editor-plugin-code-syntax-highlight/latest/toastui-editor-plugin-code-syntax-highlight-all.min.js"
-            data-navigate-track></script>
     @endpush
     <div>
         <form wire:submit="" method="POST">
             <x-card title="Register Post" shadow separator class="w-full shadow-md">
                 <div class="mb-6">
-                    <x-input wire:model="form.name" label="Title" placeholder="Title" type="text"
-                        class="no-border" />
-                </div>
-                <div class="mb-6">
-                    <x-input label="Email" wire:model="form.email" name="email" placeholder="Mail Address"
-                        type="email" class="no-border" />
+                    <x-input wire:model="name" label="Title" placeholder="Title" type="text" class="no-border" />
                 </div>
                 <div wire:ignore>
                     <div id="editor"></div>
@@ -28,10 +16,10 @@
         </form>
     </div>
     @push('scripts')
-        <script src="https://uicdn.toast.com/editor/latest/toastui-editor-all.min.js" data-navigate-track></script>
+        <script src="https://uicdn.toast.com/editor/latest/toastui-editor-all.min.js"></script>
         <script
-            src="https://uicdn.toast.com/editor-plugin-code-syntax-highlight/latest/toastui-editor-plugin-code-syntax-highlight-all.min.js"
-            data-navigate-track></script>
+            src="https://uicdn.toast.com/editor-plugin-code-syntax-highlight/latest/toastui-editor-plugin-code-syntax-highlight-all.min.js">
+        </script>
         <script>
             document.addEventListener('livewire:init', () => {
                 Livewire.hook('component.init', ({
@@ -40,41 +28,16 @@
                     const {
                         Editor
                     } = toastui;
-                    const {
-                        codeSyntaxHighlight
-                    } = Editor.plugin;
                     const editor = new Editor({
                         el: document.querySelector('#editor'),
                         initialEditType: 'markdown',
                         toolbarItems: [],
-                        plugins: [codeSyntaxHighlight],
                     });
                     editor.on('change', function(data) {
                         console.log(editor.getMarkdown());
                     })
                 })
             })
-            // document.addEventListener('livewire:init', () => {
-            //     Livewire.hook('component.init', ({
-            //         component
-            //     }) => {
-            //         const {
-            //             Editor
-            //         } = toastui;
-            //         const {
-            //             codeSyntaxHighlight
-            //         } = Editor.plugin;
-            //         const editor = new Editor({
-            //             el: document.querySelector('#editor'),
-            //             initialEditType: 'markdown',
-            //             toolbarItems: [],
-            //             plugins: [codeSyntaxHighlight],
-            //         });
-            //         editor.on('change', function(data) {
-            //             console.log(editor.getMarkdown());
-            //         })
-            //     })
-            // })
         </script>
     @endpush
 </div>
